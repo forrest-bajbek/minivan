43a55607c36fa931c6d4c46f88ac99a586ae4d49
Forrest Bajbek <forrest.bajbek@modmed.com>
2022-06-26 23:02:44 -0400

    removing more user stuff

--- src/app/models/pydantic.py
+++ src/app/models/pydantic.py
@@ -38,31 +38,3 @@ class TaskResponseSchema(TaskPayloadSchema):
                 "created_at": datetime.now(),
             }
         }
-
-
-class UserSchema(BaseModel):
-    fullname: str = Field(...)
-    email: EmailStr = Field(...)
-    password: str = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "fullname": "Abdulazeez Abdulazeez Adeshina",
-                "email": "abdulazeez@x.com",
-                "password": "weakpassword",
-            }
-        }
-
-
-class UserAuthSchema(BaseModel):
-    email: EmailStr = Field(...)
-    password: str = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "abdulazeez@x.com",
-                "password": "weakpassword",
-            }
-        }
