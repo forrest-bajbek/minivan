cf8bfdeb5c5093fd3595f780bada325b13eaa0b6
Forrest Bajbek <forrest.bajbek@modmed.com>
2022-06-26 23:05:01 -0400

    still removing old user stuff

--- src/app/main.py
+++ src/app/main.py
@@ -12,9 +12,7 @@ users = []
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router, tags=["ping"])
-    application.include_router(user.router, tags=["user"])
     application.include_router(task.router, tags=["task"])
-    application.include_router(report.router, tags=["report"])
     return application
 
 
