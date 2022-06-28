136dc8f83f84abc58fe6271d688955ce79f3bace
Forrest Bajbek <forrest.bajbek@modmed.com>
2022-06-28 15:14:41 -0400

    Added indexing to Task model

--- src/app/models/redis.py
+++ src/app/models/redis.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 
-from aredis_om import Field, JsonModel
+from aredis_om import Field, JsonModel, Migrator
 
 from app.db import redis_data
 
@@ -14,13 +14,16 @@ class BaseJsonModel(JsonModel):
 
 
 class Task(BaseJsonModel):
-    created_at: datetime = Field(...)
-    updated_at: datetime = Field(...)
-    task_app: str = Field(...)
-    task_env: str = Field(...)
-    task_name: str = Field(...)
-    task_status: str = Field(...)
-    task_watermark: datetime = Field(...)
-    task_start_at: datetime = Field(...)
+    created_at: datetime = Field(default=...)
+    updated_at: datetime = Field(default=...)
+    task_app: str = Field(default=..., index=True, full_text_search=True)
+    task_env: str = Field(default=..., index=True, full_text_search=True)
+    task_name: str = Field(default=..., index=True, full_text_search=True)
+    task_status: str = Field(default=..., index=True, full_text_search=True)
+    task_watermark: datetime = Field(default=..., index=True)
+    task_start_at: datetime = Field(default=...)
     task_stop_at: datetime | None = Field(default=None)
-    task_metadata: dict | None = Field(default=None)
+    task_metadata: dict | None = Field(default=None, index=True, full_text_search=True)
+
+
+Migrator().run()
